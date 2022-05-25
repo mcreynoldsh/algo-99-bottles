90335ef44f585e76df6bc9ba753b10f5c4bdf8e4
Hunter McReynolds <mcreynoldshd@gmail.com>
2022-05-25 12:01:27 -0500

    initial js solution

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,25 @@
-def bottle_song():
-	# write your code here!
+def bottle_song(orig_num,num_bottles):
+	next_bottle= num_bottles-1
+	body_str= f"""	{num_bottles} bottles of beer on the wall, {num_bottles} bottles of beer.
+	Take one down and pass it around, {next_bottle} bottles of beer on the wall."""
+	end_str = f"""	2 bottles of beer on the wall, 2 bottles of beer. 
+	Take one down and pass it around, 1 bottle of beer on the wall.
+	1 bottle of beer on the wall, 1 bottle of beer.
+	Take one down and pass it around, no more bottles of beer on the wall.
+	No more bottles of beer on the wall, no more bottles of beer.
+	Go to the store and buy some more, {orig_num} bottles of beer on the wall."""
+	only_one_str = """1 bottle of beer on the wall, 1 bottle of beer.
+	Take one down and pass it around, no more bottles of beer on the wall.
+	No more bottles of beer on the wall, no more bottles of beer.
+	Go to the store and buy some more, 1 bottle of beer on the wall."""
+	if num_bottles == 2:
+		return print(end_str)
+	elif num_bottles == 1:
+		return print(only_one_str)
+	else:
+		print(body_str)
+		return bottle_song(orig_num,next_bottle)	
 
+	
 
-bottle_song()
+bottle_song(15,15)
